10e1834f7a2d0966fbdd17c33b17cae716532fc8
florent guilbaud <florent.guilbaud@gmail.com>
2023-10-18 17:06:00 +0200

    adding seeds and mapboxkey

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+10.times do
+  user = User.create!(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    avater_url: Faker::Avatar.image,
+    email: Faker::Internet.email,
+    password: "123456"
+  )
+
+  # Création d'une adresse pour chaque utilisateur
+  address = Address.create!(
+    user: user,
+    city: Faker::Address.city,
+    country: Faker::Address.country,
+    line_1: Faker::Address.street_address,
+    line_2: Faker::Address.secondary_address,
+    postal_code: Faker::Address.zip_code,
+    state: Faker::Address.state,
+    phone: Faker::PhoneNumber.phone_number,
+    comment: Faker::Lorem.sentence(word_count: 5)
+  )
+
+  # Création d'un slot pour chaque utilisateur
+  Slot.create!(
+    day: Faker::Date.forward(days: 30).strftime("%A"),
+    start_hour: "#{rand(1..12)}:00 #{['AM', 'PM'].sample}",
+    end_hour: "#{rand(1..12)}:00 #{['AM', 'PM'].sample}",
+    address: address
+  )
+end
