c49ef7f713d16f77b82c6a64c9fa37f27dd19023
Francois Dumas Lattaque <dumaslattaque.francois@gmail.com>
2023-10-19 15:28:40 +0200

    Rspec - Test models user and authorization

--- spec/models/authorization_spec.rb
+++ spec/models/authorization_spec.rb
@@ -1,5 +1,22 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Authorization, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { create(:user) }
+
+  it "is valid with a unique combination of uid and provider" do
+    authorization = Authorization.new(user: user, uid: "unique_uid", provider: "unique_provider")
+    expect(authorization).to be_valid
+  end
+
+  it "is not valid with a non-unique combination of uid and provider" do
+    Authorization.create(user: user, uid: "duplicate_uid", provider: "duplicate_provider")
+    duplicate_authorization = Authorization.new(user: user, uid: "duplicate_uid", provider: "duplicate_provider")
+    expect(duplicate_authorization).not_to be_valid
+  end
+
+  it "is valid when uid is the same, but provider is different" do
+    Authorization.create(user: user, uid: "common_uid", provider: "provider1")
+    authorization = Authorization.new(user: user, uid: "common_uid", provider: "provider2")
+    expect(authorization).to be_valid
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { create(:user, first_name: "Alice", last_name: " Liddell ") }
+
+  it "create and persist a user" do
+    expect(subject).to be_valid
+  end
+
+  describe "associations" do
+    it { is_expected.to have_many(:authorizations).dependent(:destroy) }
+  end
+
+  describe "validations" do
+    it { is_expected.to validate_presence_of :email }
+    it { is_expected.to validate_presence_of :first_name }
+    it { is_expected.to validate_presence_of :last_name }
+    it { is_expected.to validate_uniqueness_of(:email).ignoring_case_sensitivity }
+  end
+
+  it "expected to be valid with an email, a password, and to be able to authenticate" do
+    expect(subject.active_for_authentication?).to be true
+  end
 end
