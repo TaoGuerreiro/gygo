718f95ae29c26bcf0ed27a388c1417bab5f0279a
Francois Dumas Lattaque <dumaslattaque.francois@gmail.com>
2023-10-23 12:14:31 +0200

    Feat - Account page setup

---
+++ spec/requests/client/account_spec.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Client::AccountController, type: :request do
+  let(:user) { create(:user) }
+
+  before do
+    address = create(:address, user:)
+    create(:slot, address:)
+    sign_in(user)
+  end
+
+  describe "GET /show" do
+    it "returns http success" do
+      get "/client/account"
+
+      expect(response).to have_http_status(200)
+    end
+  end
+end
