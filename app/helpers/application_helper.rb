3da53d64e858f5e16cd34dc35309bb94d6c02f1b
Francois Dumas Lattaque <dumaslattaque.francois@gmail.com>
2023-10-23 18:56:20 +0200

    Feat - Fire flash on turbo frame request

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def render_flash
+    return unless turbo_frame_request?
+    turbo_stream.replace "flash-alerts" do
+      render "layouts/flash"
+    end
+  end
+
+  private
+
+  def turbo_frame_request?
+    request.headers["Turbo-Frame"].present?
+  end
 end
